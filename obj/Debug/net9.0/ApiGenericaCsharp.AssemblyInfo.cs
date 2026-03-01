0c4f76ef3eee1e747eb795f1d15dd120137d4624
Juan Jose <juanjosesalazararistizabaldev@gmail.com>
2026-03-01 18:16:09 -0500

    Crear implementación ProveedorConexion que lee de appsettings.json

--- obj/Debug/net9.0/ApiGenericaCsharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiGenericaCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d23647da958bad35277341d5d3f22f82fede59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ba221c29c34710cafafb09ceaa2e57f5d4215c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
